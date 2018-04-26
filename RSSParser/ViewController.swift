d4b4df94f33de4424dc3fec7b415580647ef7e58
Tarek Sanger <tareksanger1@gmail.com>
2018-04-26 11:27:56 -0400

    Initial Commit

---
+++ RSSParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSParser
+//
+//  Created by Tarek Sanger on 2018-04-26.
+//  Copyright © 2018 Tarek Sanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
